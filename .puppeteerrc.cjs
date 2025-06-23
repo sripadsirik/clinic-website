e98ab69352c48b9b7dadcbb625fd2378f3cd1384
Sri Sirikonda <sripadsirik@gmail.com>
2025-06-23 14:34:57 -0500

    pupetteer configure

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,13 @@
+// .puppeteerrc.cjs
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // force Chrome to download (default is skipDownload: false)
+  chrome: { skipDownload: false },
+
+  // store browser builds inside your project, not in /root/.cache
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
