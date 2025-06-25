05a3acb47b29f6d6cbab986a257506ae81c94710
Sri Sirikonda <sripadsirik@gmail.com>
2025-06-24 21:57:29 -0500

    no user agent

--- src/scraper.js
+++ src/scraper.js
@@ -35,10 +35,6 @@ async function startBrowser() {
   });
 
   const page = await browser.newPage();
-  await page.setUserAgent(
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
-    '(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
-  );
   return { browser, page };
 }
 
